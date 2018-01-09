dbf202219aea82f5f53edc682350048b50dd6d13
srikanth <srikanth.bhadragiri@github.com>
2018-01-09 23:44:39 +0530

    update index.js to refer index.html as default file.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname,'dist')));
 
 app.get('/', (req, res) => {
   console.log("log index.js page, ", __dirname);
-  res.sendFile(path.join(__dirname + '/welcome.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/demo1', (req, res) => {
