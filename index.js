03d9d536371bc08e925fee24ab6fc3be05ef2e73
srikanth <srikanth.bhadragiri@github.com>
2018-01-10 07:23:52 +0530

    img path

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/countries', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/sample2.html'))
 });
 
+app.get('/img/marioq.png', (req, res) => {
+  res.sendFile(path.join(__dirname + '/img/marioq.png'))
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
