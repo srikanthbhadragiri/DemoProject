b946987c684a035d0f4f57380a26da0cb68e7be4
srikanth <srikanth.bhadragiri@github.com>
2018-02-06 01:06:03 +0530

    links

--- index.js
+++ index.js
@@ -28,6 +28,37 @@ app.get('/img/srikanth.png', (req, res) => {
   res.sendFile(path.join(__dirname + '/img/srikanth.png'))
 });
 
+app.get('/bootstrap.min.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/assets/plugins/bootstraps/css/bootstrap.min.css'))
+});
+
+app.get('/styles.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/assets/css/styles.css'))
+});
+
+// app.get('/bootstrap.min.css', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/assets/plugins/bootstraps/js/bootstrap.min.js'))
+// });
+//
+// bootstrap.min.css
+// styles.css
+// jquery-3.2.1.min.js
+// popper.min.js
+// back-to-top.js
+// jquery.scrollTo.min.js
+// jquery.easypiechart.min.js
+// bootstrap.min.js
+// sri.jpg
+// main.js
+// back-to-top.js
+// jquery.scrollTo.min.js
+// jquery.easypiechart.min.js
+// main.js
+// bootstrap.min.css
+// styles.css
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
