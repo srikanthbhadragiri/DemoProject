6e653e00cd0e2b6867343c5f248047c2628363a8
srikanth <srikanth.bhadragiri@github.com>
2018-02-05 07:47:16 +0530

    Personal Photo Change
    Updated a new photo

--- index.js
+++ index.js
@@ -18,8 +18,12 @@ app.get('/countries', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/sample2.html'))
 });
 
-app.get('/img/sri.jpg', (req, res) => {
-  res.sendFile(path.join(__dirname + '/img/sri.jpg'))
+app.get('/fullstack', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/fullstack.html'))
+});
+
+app.get('/img/srikanth.png', (req, res) => {
+  res.sendFile(path.join(__dirname + '/img/srikanth.png'))
 });
 
 const PORT = process.env.PORT || 5000;
