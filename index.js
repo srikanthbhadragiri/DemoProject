9c528aab8667557c267f393d78b53ae28b1663eb
srikanth <srikanth.bhadragiri@github.com>
2018-02-06 01:36:30 +0530

    all clear

--- index.js
+++ index.js
@@ -7,33 +7,12 @@ app.use(express.static(path.join(__dirname,'dist')));
 
 app.use(express.static('assets'));
 
-app.get('/', (req, res) => {
-  console.log("log index.js page, ", __dirname);
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/demo1', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/sample1.html'))
-});
-
-app.get('/countries', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/sample2.html'))
-});
-
-app.get('/fullstack', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/fullstack.html'))
-});
-
-app.get('/img/srikanth.png', (req, res) => {
-  res.sendFile(path.join(__dirname + '/img/srikanth.png'))
-});
-
 app.get('/bootstrap.min.css', (req, res) => {
   res.sendFile(path.join(__dirname + '/assets/plugins/bootstraps/css/bootstrap.min.css'))
 });
 
-app.get('/styles.css', (req, res) => {
-  res.sendFile(path.join(__dirname + '/assets/css/styles.css'))
+app.get('/bootstrap.min.js', (req, res) => {
+  res.sendFile(path.join(__dirname + '/assets/plugins/bootstraps/js/bootstrap.min.js'))
 });
 
 app.get('/jquery-3.2.1.min.js', (req, res) => {
@@ -52,34 +31,40 @@ app.get('/jquery.scrollTo.min.js', (req, res) => {
   res.sendFile(path.join(__dirname + '/assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js'))
 });
 
-app.get('/bootstrap.min.js', (req, res) => {
-  res.sendFile(path.join(__dirname + '/assets/plugins/bootstraps/js/bootstrap.min.js'))
+app.get('/styles.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/assets/css/styles.css'))
 });
 
 app.get('/main.js', (req, res) => {
   res.sendFile(path.join(__dirname + '/assets/js/main.js'))
 });
 
+app.get('/Srikanth_Bhadragiri.docx', (req, res) => {
+  res.sendFile(path.join(__dirname + '/assets/resume/Srikanth_Bhadragiri.docx'))
+});
 
-//
-// bootstrap.min.css
-// styles.css
-// jquery-3.2.1.min.js
-// popper.min.js
-// back-to-top.js
-// jquery.scrollTo.min.js
-// jquery.easypiechart.min.js
-// bootstrap.min.js
-// sri.jpg
-// main.js
-// back-to-top.js
-// jquery.scrollTo.min.js
-// jquery.easypiechart.min.js
-// main.js
-// bootstrap.min.css
-// styles.css
+//assets/resume/Srikanth_Bhadragiri.docx
 
+app.get('/', (req, res) => {
+  console.log("log index.js page, ", __dirname);
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
+app.get('/demo1', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/sample1.html'))
+});
+
+app.get('/countries', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/sample2.html'))
+});
+
+app.get('/fullstack', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/fullstack.html'))
+});
+
+app.get('/sri.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname + '/assets/images/sri.jpg'))
+});
 
 const PORT = process.env.PORT || 5000;
 
